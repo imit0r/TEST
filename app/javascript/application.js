b9a1f351009cfac0faf495b7bf4556f55ac65927
imit0r <rotimigbafe@gmail.com>
2023-04-26 09:11:48 +0100

    fixing navbar problems(webpack is an outdated feature so, using alternatives)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,9 +2,10 @@
 //= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap.min
+//= require bootstrap
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
 import "popper"
 import "bootstrap"
+
