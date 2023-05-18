db5f649d92f9772aa606dd9498f321a91d056554
imit0r <rotimigbafe@gmail.com>
2023-05-18 13:20:26 +0100

    Layout Clean-up, wrap- up section 5

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,9 +2,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//=
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "popper"
-import "bootstrap"
-
